e8f252f35954ccd1199e9694cb3f96fb06d8fcc1
Amar Panigrahy <amar.panigrahy@gmail.com>
2018-09-22 13:53:15 +0530

    FEATURE: Improvised command line parameter usage
    Used jsoup Validate feature to accept the starting URL to crawl

--- src/main/java/com/amar/webcrawler/BootWebCrawlerApplication.java
+++ src/main/java/com/amar/webcrawler/BootWebCrawlerApplication.java
@@ -1,13 +1,13 @@
 package com.amar.webcrawler;
 
 import com.amar.webcrawler.service.CrawlService;
+import org.jsoup.helper.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 public class BootWebCrawlerApplication implements CommandLineRunner {
@@ -23,13 +23,9 @@ public class BootWebCrawlerApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        String startUrlToCrawl = args[0];
-        if (StringUtils.isEmpty(startUrlToCrawl)) {
-            LOGGER.error("Please provide URL as command line argument to start crawling");
-            return;
-        }
-
-        crawlService.crawl(startUrlToCrawl);
+        Validate.isTrue(args.length == 1,
+                        "Usage: supply URL as command line parameter to start crawling");
+        crawlService.crawl(args[0]);
     }
 
     public CrawlService<String> getCrawlService() {
